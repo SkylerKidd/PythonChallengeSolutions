e8230cf8a08a5d333f0b81275a8111065bca8cf8
Skyler Kidd <f.s.kidd@gmail.com>
2015-06-18 10:19:08 -0700

    Solution to 4

---
+++ Ch_4.py
@@ -0,0 +1,29 @@
+# Python Challenge
+# Solution to challenge 3
+
+import urllib.request as ur
+
+def findNothing(current):
+    urlBeg = 'http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing='
+    response = ur.urlopen(urlBeg + current)
+    data = response.read().decode('utf-8')
+    for i in range(400):
+        nothing = ""
+        for char in data[len(data)-1:len(data)-6:-1]:
+            if char >= '0' and char <= '9':
+                nothing = char + nothing
+        if nothing == "":
+            break
+        response = ur.urlopen(urlBeg + nothing)
+        print (urlBeg + nothing)
+        data = response.read().decode('utf-8')
+        print (data)
+
+findNothing('12345')
+
+# Result:               'Yes. Devide by two and keep going.'
+# Current 'nothing':    16044
+
+findNothing('8022')
+
+# Result:               'peak.html'
